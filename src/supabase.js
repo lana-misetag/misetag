017156931bec5135eadfe14ff87883945aa44c65
lana-misetag <svit.katiukha@gmail.com>
2026-04-14 00:44:08 -0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
-
+ 
 const supabaseUrl = 'https://aklptqxgvbertqwgdjoz.supabase.co'
 const supabaseKey = 'sb_publishable_1d2nbiC-0NMv25Txm-EWGQ_ub5N540T'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+ 
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+})
